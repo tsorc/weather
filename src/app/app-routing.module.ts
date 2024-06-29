1a0b5781ad777443d3e2d758fa292193cc921814
Tilen <tilen.sorc@gmail.com>
2024-06-29 12:40:01 +0200

    Refactored redirect in routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {IndexComponent} from "./index/index.component";
 const routes: Routes = [
   { path: '', redirectTo: 'index', pathMatch: 'full' },
   { path: 'index', component: IndexComponent },
-  { path: '**', component: IndexComponent } // catch all others
+  { path: '**', redirectTo: 'index' } // catch all others
 ];
 
 
